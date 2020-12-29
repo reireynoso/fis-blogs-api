a197bead464f90cb77340a2b9ec1b547194d5ee3
Reinald Reynoso <reireynoso@gmail.com>
2020-12-29 17:48:13 -0500

    scape data for blog implemented

--- routes/Blog.js
+++ routes/Blog.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = new express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 router.get("/testing", (req,res) => {
     try {
@@ -7,6 +9,18 @@ router.get("/testing", (req,res) => {
     } catch (error) {
         res.send("error")
     }
+})
+
+router.get("/blog/new", async(req,res) => {
+    const {data} = await axios.get("https://medium.com/@reireynoso/drag-ndrop-with-react-beautiful-dnd-73014e5937f2") 
+    const $ = cheerio.load(data);
+    // find the post image. By default it returns with size 60. Replace is switching it with a higher resolution
+    // default sample: https://miro.medium.com/max/60/some_image.png?q=20
+    // console.log($('img[alt="Image for post"]').attr('src').replace("60", "6000"));
+    const image = $('img[alt="Image for post"]').attr('src').replace("60", "728");
+    const title = $('meta[property="og:title"]').attr('content')
+    console.log(title)
+    res.send({image})
 })
 
 
--- routes/Cohort.js
+++ routes/Cohort.js
@@ -5,10 +5,15 @@ const auth = require('../middleware/auth');
 
 router.get("/cohort", async(req,res) => {
 
-    const cohorts = await Cohort.find({}).select('-_id').populate('admins'); // excludes the _id property
+    const cohorts = await Cohort.find({}).populate('admins'); // excludes the _id property
 
     res.send(cohorts);
 })
+
+// router.get("/cohort/admin", async(req,res) => {
+//     const cohorts = await Cohort.find({}).select('-_id').populate('admins');
+//     res.send(cohorts);
+// })
 
 router.post("/cohort/new", auth, async(req,res) => {
     // check if user is admin. if not, throw error
