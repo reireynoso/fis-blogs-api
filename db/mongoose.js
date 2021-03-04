504ff350f5db474ddb68554d7e8798f32ba8b043
Reinald Reynoso <reireynoso@gmail.com>
2021-03-03 23:24:40 -0500

    implemented local database for development

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async() => {
     try{
-        const conn = await mongoose.connect(process.env.MONGODB_URL, {
+        const conn = await mongoose.connect(process.env.NODE_ENV === "production" ? process.env.MONGODB_URL : 'mongodb://127.0.0.1:27017/codingBlogs-api', {
             useNewUrlParser: true,
             useCreateIndex: true,
             useFindAndModify: false,
