2f516032d4fc1975d75a1015a3d7e0f1aed97d37
Reinald Reynoso <reireynoso@gmail.com>
2021-03-07 19:58:37 -0500

    cleaned code and added tags

--- routes/Cohort.js
+++ routes/Cohort.js
@@ -4,18 +4,6 @@ const Cohort = require('../models/Cohort');
 const auth = require('../middleware/auth');
 const User = require('../models/User');
 
-// router.get("/cohort", async(_,res) => {
-
-//     const cohorts = await Cohort.find({}).populate('admins'); // excludes the _id property
-
-//     res.send(cohorts);
-// })
-
-// router.get("/cohort/admin", async(req,res) => {
-//     const cohorts = await Cohort.find({}).select('-_id').populate('admins');
-//     res.send(cohorts);
-// })
-
 router.post("/cohort/new", auth, async(req,res) => {
     // check if user is admin. if not, throw error
     try {
